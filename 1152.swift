1a47aa726ae6dc76f4a33271cd5673391ecf1ae7
정하림 <109908037+jnghrmi@users.noreply.github.com>
2022-08-11 23:58:43 +0900

    Add files via upload

---
+++ 1152.swift
@@ -0,0 +1,15 @@
+//
+//  1152.swift
+//  ios study
+//
+//  Created by 정하림 on 2022/08/10.
+//
+
+import Foundation
+
+let input = readLine()
+
+if let input = input {
+    let array = input.split(separator: " ")
+    print(array.count)
+}
